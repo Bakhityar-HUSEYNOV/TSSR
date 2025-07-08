8050b78b5d155439768e04337e74d57e59fb4ea6
MC-Baki <bakhtiyar.huseynov@proton.me>
2025-07-08 14:17:59 +0200

    changed static IP to ipconfig one

--- test.ps1
+++ test.ps1
@@ -1,6 +1,6 @@
 $test = "I love Minecraft"
 
-$ip = ("255.255.255.255")
+$ip = (ipconfig | Select-String 'IPv4' | Select-Object -First 1).ToString().Split(':')[-1].Trim()
 if ($ip -match '^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]?[0-9])){3})$') {
     Write-Output "Valid IPv4 address: $ip"
 } else {
