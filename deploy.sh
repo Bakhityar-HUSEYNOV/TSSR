5f24afe107307c3e9485264d8fdb493456805763
MC-Baki <bakhtiyar.huseynov@proton.me>
2025-07-15 15:50:07 +0200

    changed colors;final touches

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #UNCOMMENT THE FOLLOWING LINES BEFORE PROD
+RED='\033[1;31m' # Red
+GREEN='\033[1;32m' # Green
 YELLOW='\033[1;33m' # Yellow
 NC='\033[0m' # No Color
 if [[ $EUID -ne 0 ]]; then
@@ -18,11 +20,11 @@ if [ $? -ne 0 ] ; then
     apt-get update && apt install -y curl >>logs/tailscale_install.log
 
     if [ $? -eq 0 ] ; then
-        echo -e "${YELLOW}Installed curl successfully, proceeding with Tailscale installation...${NC}" 
+        echo -e "${GREEN}Installed curl successfully, proceeding with Tailscale installation...${NC}" 
         echo -e "${NC}DEBUG: Installed curl successfully, proceeding with Tailscale installation" >>logs/tailscale\install.log
     else
-        echo -e "${YELLOW}Failed to install curl, check the logs for errors" 
-        echo -e "${YELLOW}DEBUG: Failed to install curl, check the logs for errors" >>logs/tailscale_install.log
+        echo -e "${RED}Failed to install curl, check the logs for errors" 
+        echo -e "${RED}DEBUG: Failed to install curl, check the logs for errors" >>logs/tailscale_install.log
         exit 1
     fi
 fi
@@ -33,20 +35,20 @@ fi
 dpkg -l tailscale >> /dev/null 2>&1
 if [ $? -eq 0 ] ; then
     TAILSCALE_INSTALLED="true"
-    echo -e "${YELLOW}Tailscale is already installed "
-    echo -e "${YELLOW}DEBUG: Tailscale is already installed " >>logs/tailscale_install.log
+    echo -e "${GREEN}Tailscale is already installed "
+    echo -e "${GREEN}DEBUG: Tailscale is already installed " >>logs/tailscale_install.log
 else
     TAILSCALE_INSTALLED="false"
     echo -e "${YELLOW}Tailscale is not installed, installing now... ${NC}"  
     echo -e "DEBUG: Tailscale is not installed, installing now..." >>logs/tailscale_install.log 
     curl -fsSL https://tailscale.com/install.sh |sh >>logs/tailscale_install.log 2>&1 
     if [ $? -eq 0 ] ; then
-        echo -e "${YELLOW}Installed Tailscale"
+        echo -e "${GREEN}Installed Tailscale"
         echo -e "DEBUG: Installed Tailscale" >>logs/tailscale_install.log
 
     else
-        echo -e "${YELLOW}Failed to install Tailscale, check the logs for details"
-        echo -e "${YELLOW}Failed to install Tailscale, check the logs for details" >>logs/tailscale_install.log
+        echo -e "${RED}Failed to install Tailscale, check the logs for details"
+        echo -e "${RED}Failed to install Tailscale, check the logs for details" >>logs/tailscale_install.log
         exit 1
     fi
 fi
@@ -56,8 +58,8 @@ if tailscale status | grep -q "Logged out" ; then
     echo -e "${YELLOW}Tailscale is logged out, attempting to authenticate..." 
     echo -e "${YELLOW}DEBUG:Tailscale is logged out, attempting to authenticate..." >>logs/tailscale_install.log
 else
-    echo -e "${YELLOW}Tailscale is already authenticated, no need to log in again"
-    echo -e "${YELLOW}DEBUG:Tailscale is already authenticated, no need to log in again" >>logs/tailscale_install.log
+    echo -e "${GREEN}Tailscale is already authenticated, no need to log in again"
+    echo -e "${GREEN}DEBUG:Tailscale is already authenticated, no need to log in again" >>logs/tailscale_install.log
     exit 0
 fi
 
@@ -69,16 +71,16 @@ if [ -f ./top_secret ] ; then
     echo -e "${YELLOW}Using auth key: $TAILSCALE_AUTH_KEY" >>logs/tailscale_install.log
     tailscale up --authkey $TAILSCALE_AUTH_KEY >>logs/tailscale_install.log
     if [ $? -eq 0 ] ; then
-        echo -e "${YELLOW}Tailscale authenticated successfully"
-        echo -e "${YELLOW}Tailscale is now running, your ip address is: $(tailscale ip -4)"
-        echo -e "${YELLOW}Tailscale is now running, your ip address is: $(tailscale ip -4)" >>logs/tailscale_install.log
+        echo -e "${GREEN}Tailscale authenticated successfully"
+        echo -e "${GREEN}Tailscale is now running, your ip address is: $(tailscale ip -4)"
+        echo -e "${GREEN}Tailscale is now running, your ip address is: $(tailscale ip -4)" >>logs/tailscale_install.log
     else
-        echo -e "${YELLOW}DEBUG: Failed to authenticate Tailscale, check the logs for details" >>logs/tailscale_install.log
-        echo -e "${YELLOW}Failed to authenticate Tailscale, check the logs for details" 
+        echo -e "${RED}DEBUG: Failed to authenticate Tailscale, check the logs for details" >>logs/tailscale_install.log
+        echo -e "${RED}Failed to authenticate Tailscale, check the logs for details" 
         exit 1
     fi
 else
-    echo -e "${YELLOW}No secret auth key found, please provide one in top_secret.txt"
-    echo -e "${YELLOW}DEBUG: No secret auth key found, please provide one in top_secret.txt" >>logs/tailscale_install.log
+    echo -e "${RED}No secret auth key found, please provide one in top_secret.txt"
+    echo -e "${RED}DEBUG: No secret auth key found, please provide one in top_secret.txt" >>logs/tailscale_install.log
     exit 1
 fi
