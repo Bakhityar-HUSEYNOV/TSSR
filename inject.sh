cb45ad99445379187ccde089f1b18fbd37156e4d
MC-Baki <bakhtiyar.huseynov@proton.me>
2025-12-12 13:39:18 +0100

    adding echo for user

--- inject.sh
+++ inject.sh
@@ -38,4 +38,17 @@ EOF
 umount "$TARGET_MOUNT_POINT" 
  
 #Waiting for user input to close FOS
+echo "Injection completed.Files and Service Configured."
+echo "Files Copied To:"
+echo "  - Script: $TARGET_MOUNT_POINT/var/my-firstboot/my-script.sh"
+echo "  - Key:    $TARGET_MOUNT_POINT/var/my-firstboot/key.txt"
+echo " "
+echo "Systemd Service Status:"
+echo "  - File Created: $TARGET_MOUNT_POINT/etc/systemd/system/my-firstboot.service"
+echo "  - Target:       multi-user.target"
+echo "  - Dependency:   After=multi-user.target (Latest possible run time)"
+echo "  - Log Output:   StandardOutput=journal (Logs viewable via 'journalctl -u my-firstboot.service')"
+echo "  - Status:       Partition $TARGET_MOUNT_POINT mounted, configured, and ready for first boot."
+echo "========================================================="
+echo " Press Enter to reboot"
 read
